602ad5f696b23fafcf635fe23f8611bde492a388
Ruslan Koshevoy <ruslan.koshevoj@mail.ru>
2017-03-08 13:46:25 +0200

    Revert "add development version"
    This reverts commit 8a2000b3d332eafbe1bbadb89ba18f88fdc96f4b.

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,14 +1,1 @@
-$(document).ready(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
+$(document).ready(function(){$('a[href*="#"]:not([href="#"])').click(function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var t=$(this.hash);if(t=t.length?t:$("[name="+this.hash.slice(1)+"]"),t.length)return $("html, body").animate({scrollTop:t.offset().top},1e3),!1}})});
